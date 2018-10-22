1fb1aae6127c9ccd694d495019e84f0c5055b771
Sean Leather <sean.leather@gmail.com>
2018-10-22 10:43:27 +0200

    Initial formulae: kpathsea, dvips

---
+++ Formula/dvips.rb
@@ -0,0 +1,57 @@
+class Dvips < Formula
+  desc "Convert DVI to PostScript"
+  homepage "https://tug.org/dvips/"
+  # A local mirror but not a permanent URL. Try it first.
+  url "http://mirror.ctan.org/systems/texlive/Source/texlive-20180414-source.tar.xz"
+  # Not a local mirror but is a permanent URL. Try it second.
+  mirror "ftp://tug.org/historic/systems/texlive/2018/texlive-20180414-source.tar.xz"
+  sha256 "fe0036d5f66708ad973cdc4e413c0bb9ee2385224481f7b0fb229700a0891e4e"
+  version "5.998"
+
+  depends_on "pkg-config" => :build
+  depends_on "kpathsea"
+
+  def install
+    chdir "texk/dvipsk" do
+      # Configure, build, install
+      system "./configure",
+        "--disable-dependency-tracking",
+        "--disable-silent-rules",
+        "--enable-shared",
+        "--with-system-kpathsea",
+        "--prefix=#{prefix}"
+      system "make", "install"
+
+      # This path is required for post_install.
+      pkgshare.install share/"texmf-dist"
+
+      # Save a file for the test.
+      (prefix/"test").install "testdata/eepic-nan.dvi"
+    end
+  end
+
+  # This is the shared distribution configuration directory.
+  def texmf_dist
+    HOMEBREW_PREFIX/"share/texmf-dist"
+  end
+
+  def post_install
+    # Create symlinks for all the TeX formulae installed.
+    texmf_dist.mkpath
+    chdir texmf_dist do
+      Dir["../*/texmf-dist/*"].each do |p|
+        ln_sf p, "."
+      end
+    end
+
+    # Update the kpathsea filename database (ls-R files):
+    # https://tug.org/texinfohtml/kpathsea.html#Filename-database
+    system "mktexlsr"
+  end
+
+  test do
+    # If this test passes, then kpathsea is working and dvips can correctly
+    # process a file.
+    system bin/"dvips", prefix/"test/eepic-nan.dvi"
+  end
+end
---
+++ Formula/kpathsea.rb
@@ -0,0 +1,101 @@
+class Kpathsea < Formula
+  desc "Path searching library and tools for TeX-related files"
+  homepage "https://tug.org/kpathsea/"
+  # A local mirror but not a permanent URL. Try it first.
+  url "http://mirror.ctan.org/systems/texlive/Source/texlive-20180414-source.tar.xz"
+  # Not a local mirror but is a permanent URL. Try it second.
+  mirror "ftp://tug.org/historic/systems/texlive/2018/texlive-20180414-source.tar.xz"
+  sha256 "fe0036d5f66708ad973cdc4e413c0bb9ee2385224481f7b0fb229700a0891e4e"
+  version "6.3.0"
+
+  def install
+    chdir "texk/kpathsea" do
+      # SELFAUTOLOC is a variable defined by kpathsea that tells a binary using
+      # kpathsea (e.g. dvips, xdvi, dvisvgm, etc.) the location of that binary.
+      # Since Homebrew installs formulae into unique directories and does not
+      # follow the TeX Directory Structure (TDS), we set SELFAUTOLOC to the the
+      # location of the installed symlink rather than the actual binary path.
+      #
+      # Sources:
+      #   * https://tobywf.com/2017/04/build-dvisvgm-kpathsea-on-macos/
+      #   * https://gist.github.com/tobywf/aeeeee63053aaaa841b4032963406684
+      inreplace "progname.c",
+        /kpathsea_selfdir *\(kpse, *kpse->invocation_name\)/,
+        "xstrdup (\"#{HOMEBREW_PREFIX}/bin\")"
+
+      # texmf.cnf contains a large number of search paths and directories used
+      # by kpathsea. We adapt these to fit the Homebrew directory structure.
+
+      inreplace "texmf.cnf" do |s|
+        # TEXMFROOT is the root directory for a TeXLive distribution (assuming
+        # TDS). TEXMFROOT includes the distribution directory TEXMFDIST (which
+        # is correct in texmf.cnf) and the directory for local additions
+        # TEXMFLOCAL (which is left for users and explicitly not used by
+        # Homebrew).
+
+        # TEXMFROOT is so-named because it is the root of a portable TeX Live
+        # distribution. However, we use a directory hierarchy specifically for
+        # Homebrew, which is not portable.
+        s.gsub! /^(TEXMFROOT) *= *(.*)/, "\\1 = #{HOMEBREW_PREFIX}/share % was: \\2"
+
+        # TEXMFDIST is reserved for distribution configuration files. It is use
+        # by Homebrew formula and should not be changed by users. The default
+        # value is good: $TEXMFROOT/texmf-dist
+
+        # TEXMFLOCAL is reserved for users. Configuration files in this
+        # directory override configuration files in TEXMFDIST.
+        s.gsub! /^(TEXMFLOCAL) *= *(.*)/, "\\1 = $TEXMFROOT/texmf-local % was: \\2"
+
+        # TEXMFSYSVAR is where *-sys store cached runtime data.
+        s.gsub! /^(TEXMFSYSVAR) *= *(.*)/, "\\1 = #{var}/texmf % was: \\2"
+
+        # TEXMFSYSCONFIG is where *-sys store configuration data.
+        s.gsub! /^(TEXMFSYSCONFIG) *= *(.*)/, "\\1 = #{etc}/texmf % was: \\2"
+      end
+
+      # Configure, build, install
+      system "./configure",
+        "--disable-dependency-tracking",
+        "--disable-silent-rules",
+        "--enable-shared",
+        "--prefix=#{prefix}"
+      system "make", "install"
+
+      # This path is required for post_install.
+      pkgshare.install share/"texmf-dist"
+    end
+  end
+
+  # This is the shared distribution configuration directory.
+  def texmf_dist
+    HOMEBREW_PREFIX/"share/texmf-dist"
+  end
+
+  def post_install
+    # Create symlinks for all the TeX formulae installed.
+    texmf_dist.mkpath
+    chdir texmf_dist do
+      Dir["../*/texmf-dist/*"].each do |p|
+        ln_sf p, "."
+      end
+    end
+  end
+
+  def caveats; <<~EOS
+    This formula creates the shared directory #{texmf_dist}
+    for all formulae using kpathsea.
+
+    After uninstalling kpathsea, you may which to remove this directory, which
+    is no longer used:
+
+      rm -rf #{texmf_dist}
+
+  EOS
+  end
+
+  test do
+    # This test passes if kpathsea knows how to find the configuration file,
+    # which is a minimum for every binary built with kpathsea.
+    system bin/"kpsewhich", "texmf.cnf"
+  end
+end
