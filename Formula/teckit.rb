265c49fd694a89be3d899b7043400c77100ed16e
Sean Leather <sean.leather@gmail.com>
2018-10-26 14:21:49 +0200

    Add formula: teckit

---
+++ Formula/teckit.rb
@@ -0,0 +1,43 @@
+class Teckit < Formula
+  desc "Text Encoding Conversion toolkit"
+  homepage "https://github.com/silnrsi/teckit"
+  url "https://github.com/silnrsi/teckit/archive/v2.5.8.tar.gz"
+  sha256 "0ea52b1304f430aaebff99fec355bc7e4ad75b16ba58959fae5079627f925f93"
+  #url "https://github.com/silnrsi/teckit/releases/download/v2.5.8/teckit-2.5.8.tar.gz"
+  #sha256 "35df1f68f1083791a884b12947bfab4de2cd20bac8f36793ed6638da2f24be01"
+
+  depends_on "libtool" => :build
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "expat"
+
+  def testdata
+    prefix/"test"
+  end
+
+  def install
+    # Generate ./configure
+    system "./autogen.sh"
+    # Configure, build, install
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--enable-shared",
+      "--disable-static",
+      "--enable-final",
+      "--prefix=#{prefix}"
+    system "make", "install"
+
+    # Save files for the test.
+    testdata.install Dir["test/{SILGreek2004-04-27.map,SILGreek2004-04-27.uncompressed.tec.orig}"]
+  end
+
+  test do
+    # This is the first test taken from the teckit tests.
+    system bin/"teckit_compile", testdata/"SILGreek2004-04-27.map", "-z", "-o",
+      "SILGreek.uncompressed.tec"
+    assert compare_file testdata/"SILGreek2004-04-27.uncompressed.tec.orig",
+      testpath/"SILGreek.uncompressed.tec"
+  end
+end
